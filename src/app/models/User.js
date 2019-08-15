3f3aaf3b9f34da5691042df3f079772436d98a5a
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-14 22:34:11 -0400

    Hash de senhas

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   // -> classe que sera automaticamente chamada na inicializacao
@@ -6,8 +7,10 @@ class User extends Model {
     super.init(
       {
         // -> os campos abaixo representao quais receberao dados ou alteracoes
+        // -> O Campo password é virtual e nao esta presente no model de User
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -15,6 +18,13 @@ class User extends Model {
         sequelize,
       }
     );
+    // -> Metodo antes de salvar
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    return this;
   }
 }
 export default User;

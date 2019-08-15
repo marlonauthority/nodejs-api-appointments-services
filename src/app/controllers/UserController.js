b3892b06e08316db83d8193921ab8b3946241499
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-15 16:28:14 -0400

    Update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,8 +21,39 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    // -> pagamos os campos do body
+    const { email, oldPassword } = req.body;
+    // -> buscamos o user usando o primary key
+    const user = await User.findByPk(req.userId);
+
+    // -> Caso houver o email
+    if (email !== user.email) {
+      // Verificar se existe email no DB
+      const userExists = await User.findOne({
+        where: { email },
+      });
+      // caso exista o email, enta no if
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuário já existe.' });
+      }
+    }
+
+    // caso foi informado o campo oldpassword, cairá aqui
+    // -> Caso a senha Old bate com a atual
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senhas não coincidem' });
+    }
+
+    // se passou pelas verificacoes, att o user
+    const { id, name, provider } = await user.update(req.body);
+
+    // -> Retorna os dados repassados
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 

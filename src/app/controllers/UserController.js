0a8316fd62d521633aa0c50ae0612768c17d4c2b
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-14 21:58:53 -0400

    store Users

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,24 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // -> Verificar se existe email no DB
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    // caso exista o email, enta no if
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já existe.' });
+    }
+    // -> Cria um user e apenas os dados repassados
+    const { id, name, email, provider } = await User.create(req.body);
+
+    // -> Retorna os dados repassados
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Marlon Authority',
-    email: 'marlon_authority@msn.com',
-    password_hash: '12345678910',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;

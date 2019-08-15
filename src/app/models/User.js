5bccbb20f4a4224a2fc669ae2ddee50d4c359614
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-14 21:33:50 -0400

    loader dos models

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,9 +1,11 @@
 import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
+  // -> classe que sera automaticamente chamada na inicializacao
   static init(sequelize) {
     super.init(
       {
+        // -> os campos abaixo representao quais receberao dados ou alteracoes
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,

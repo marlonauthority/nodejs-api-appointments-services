416c2cccf20445338257c5e3116cd686da777cda
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-17 18:19:24 -0400

    cancelamento do agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 import User from '../models/User';
 import File from '../models/File';
@@ -109,6 +109,55 @@ class AppointmentController {
       content: `Novo agendamento feito por ${user.name}, para o ${formatedDate}`,
       user: provider_id,
     });
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+    // -> caso quem esta tentando cancelar o agendamento nao for o dono do agendamento..
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'Você naão tem permissão para cancelar este agendamento.',
+      });
+    }
+    // -> Se ja foi cancelado emita o aviso
+    if (appointment.canceled_at !== null) {
+      return res.status(401).json({
+        error: 'Este agendamento já foi cancelado.',
+      });
+    }
+    // -> Só será possível cancelar o agendamento estando com 2 horas de antecedencia
+    // remove 2 horas do agendamento feito
+    const dataWithSub = subHours(appointment.date, 2);
+    // -> Exemplo
+    //  now: 11:00 -> If abaixo pega o horario atual, aqui eu exemplifico como sendo 1 hora antes do agendamento
+    //  appointment.date: 12:00 -> Horario agendado no DB
+    //  dataWithSub: 10:00 -> Novo horario feito pela constante criada acima
+    // Neste exemplo nao sera possivel cancelar por que no horario atual ja passam do horario limite 2 de horaas antescedentes para cancelar
+    if (isBefore(dataWithSub, new Date())) {
+      return res.status(401).json({
+        error:
+          'Você só pode cancelar o agendamento, estando à 2 horas de antecedencia.',
+      });
+    }
+    // -> se estiver tudo certo
+    appointment.canceled_at = new Date();
+    await appointment.save();
+    //
+    // -> Beleza, agendamento feito que tal uma notificação para o prestador de servico
+    const user = await User.findByPk(req.userId);
+    const formatedDate = format(
+      appointment.date,
+      "'dia' dd 'de' MMMM', para às' H'h'",
+      {
+        locale: pt,
+      }
+    );
+    await Notification.create({
+      content: `${user.name}, cancelou o agendamento do ${formatedDate}`,
+      user: appointment.provider_id,
+    });
+
     return res.json(appointment);
   }
 }

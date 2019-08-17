73c076066f65f4c55fd5fbecd51813616a16235b
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-17 17:02:27 -0400

    Listagem notificacoes para prestadores

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    // -> Verificar se o usuario logado é um prestador
+    const checkIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (!checkIsProvider) {
+      return res.status(401).json({
+        error:
+          'Somente prestadores de serviçoes podem vizualizar as notificações!',
+      });
+    }
+    // -> Listar as notificacoes
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort('-createdAt')
+      .limit(20);
+    return res.json(notifications);
+  }
+}
+
+export default new NotificationController();

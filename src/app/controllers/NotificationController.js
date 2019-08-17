5fc4c756552901f26afb314573cf4f5c571bc3f7
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-17 17:15:27 -0400

    Notificacao Lida

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,6 +21,19 @@ class NotificationController {
       .limit(20);
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    // -> existem duas formas a primeira abaixo "comentada"
+    // const notification = await Notification.findById(req.params.id);
+    // nesta 2ª forma passamos um id no comeco do array e como segundo o campo a ser modificado
+    // new true, retorna a informacao atualizada
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
+  }
 }
 
 export default new NotificationController();
